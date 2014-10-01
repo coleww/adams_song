39de813090b5fbd3ad6a0c82aea645a4cd2f6d5f
Cole Willsea <coleww@gmail.com>
2014-10-01 08:36:25 -0700

    string#adams_song?

--- lib/adams/song.rb
+++ lib/adams/song.rb
@@ -1,7 +1,18 @@
 require "adams/song/version"
 
-module Adams
-  module Song
-    # Your code goes here...
+class String
+
+  def adams_song?
+    that = self.downcase.gsub(/[^a-zA-Z\d\s]/, '')
+    !!(that.match /i?\s?(cant|couldnt)\s+wait\s+(till|until)\s+i\s+(got|get)\s+home\s+to\s+pass\s+the\s+time\s+in\s+my\s+room\s+alone/) ||
+    !!(that.match /i?\s?never\s+thought\s+id\s+die\s+alone\s+(i laughed the loudest|another six months)\s+(whod have known|ill be unknown)/) ||
+    !!(that.match /i?\s?never\s+conquered\s+rarely\s+came\s+(tomorrow|16 just)\s+(held|holds)\s+such\s+better\s+days/) ||
+    !!(that.match /days\s+when\s+i\s+(can still|still)\s+(feel|felt)\s+alive\s+(when i|we)\s+(couldnt|cant)\s+wait\s+to\s+get\s+outside/) ||
+    !!(that.match /i\s+traced\s+the\s+cord\s+back\s+to\s+the\s+wall\s+no\s+wonder\s+it\s+was\s+never\s+plugged\s+in\s+at\s+all/) ||
+    !!(that.match /i took my time i hurried up the choice was mine i didnt think enough/) || # i am bored now. this is boring.
+    !!(that.match /im too depressed to go on youll be sorry when im gone/) ||
+    !!(that.match /give all my things to all my friends youll never step foot in my room again/) ||
+    !!(that.match /remember the time that i spilled the cup of apple juice in the hall please tell mom this is not her fault/)
+
   end
 end
---
+++ test/adams_song_test.rb
@@ -0,0 +1,94 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/adams/song.rb'
+
+class TestStringAdamsSong <  Minitest::Test
+
+  def test_knows_cant_wait
+    assert_equal true, "I can't wait till I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_knows_a_fuzzy_cant
+    assert_equal true, "I cant wait till I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_accepts_couldnt
+    assert_equal true, "I couldnt wait till I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_knows_about_until
+    assert_equal true, "I cant wait until I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_knows_about_got
+    assert_equal true, "I can't wait till I got home to pass the time in my room alone".adams_song?
+  end
+
+  def test_knows_omissions_are_cool_sometimes
+    assert_equal true, "can't wait till I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_dgafs_about_punctuations
+    assert_equal true, "I can't wait, till I get home/ to pass the time: in my room alone".adams_song?
+  end
+
+  def test_knows_what_is_not_it
+    assert_equal false, "I dont want to go to school".adams_song?
+  end
+
+  def test_accepts_things_with_hashtags
+    assert_equal true, "#watlol I can't wait till I get home to pass the time in my room alone".adams_song?
+  end
+
+  def test_accepts_things_with_urls
+    assert_equal true, "I can't wait till I get home to pass the time in my room alone http://google.com".adams_song?
+  end
+
+  def test_accepts_things_with_twitterish_urls
+    assert_equal true, "I can't wait till I get home to pass the time in my room alone tmblr.co/g00gL3".adams_song?
+  end
+
+  def test_knows_laughed_loudest
+    assert_equal true, "i never thought id die alone i laughed the loudest whod have known".adams_song?
+  end
+
+  def test_knows_be_unknown
+    assert_equal true, "i never thought id die alone another six months ill be unknown".adams_song?
+  end
+
+  def test_knows_tomorrow
+    assert_equal true, "i never conquered rarely came tomorrow holds such better days".adams_song?
+  end
+
+  def test_knows_sixteen
+    assert_equal true, "i never conquered rarely came 16 just held such better days".adams_song?
+  end
+
+  def test_knows_we_couldnt
+    assert_equal true, "Days when i still felt alive we couldnt wait to get outside".adams_song?
+  end
+
+  def test_knows_i_couldnt
+    assert_equal true, "Days when i can still feel alive when i cant wait to get outside".adams_song?
+  end
+
+  def test_knows_traced_cord
+    assert_equal true, "i traced the cord back to the wall no wonder it was never plugged in at all".adams_song?
+  end
+
+  def test_knows_took_time
+    assert_equal true, "i took my time i hurried up the choice was mine i didnt think enough".adams_song?
+  end
+
+  def test_knows_too_depressed
+    assert_equal true, "im too depressed to go on youll be sorry when im gone".adams_song?
+  end
+
+  def test_knows_all_my_thing
+    assert_equal true, "Give all my things to all my friends youll never step foot in my room again".adams_song?
+  end
+
+  def test_knows_apple_juice
+    assert_equal true, "Remember the time that i spilled the cup Of apple juice in the hall Please tell mom this is not her fault".adams_song?
+  end
+end
